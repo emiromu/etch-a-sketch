7b6c6c381ac7faa39e19ba0bf5f9c0bfc85197c5
Emilien Romulus <emilien.romulus@tuta.io>
2021-05-24 12:32:51 +1000

    Changed the canvas size to Square only; changed from Black and White to random color

--- game.js
+++ game.js
@@ -15,9 +15,24 @@ var noRow=4;
 
 //Colour an element's background to black
 function colourblack(el){
-    el.style.backgroundColor="black";;
+    el.style.backgroundColor="black";
 };
 
+//returns a random rgb(#,#,#) string
+function generateRandomColour(){
+    var r = Math.random()*254 +1;
+    var g = Math.random()*254 +1;
+    var b = Math.random()*254 +1;
+
+    return `rgb(${r},${g},${b})`;
+};
+
+//Colour an element's background to random
+function colourrandom(el){
+    el.style.backgroundColor=generateRandomColour();
+};
+
+//Setup the sketch canvas noCol x noRow
 function setup(noCol, noRow){
 
     //remove existing
@@ -42,7 +57,7 @@ function setup(noCol, noRow){
             columns[i].appendChild(cells[i+j]);   
             //mouse hover event
             document.getElementById("cell"+i+";"+j).addEventListener("mouseover",function(){
-                colourblack(this);
+                colourrandom(this);
             });
         };
     };
@@ -52,12 +67,22 @@ eraser.addEventListener("click",function(){
     var noCol=0;
     var noRow=0;
 
+    //NOT square
+    /*
     while(noCol<=0 || noCol>100 || noRow<=0 || noRow>100){
         noCol = prompt(`Enter number of columns
         (number between 1 and 100)`,4);
         noRow = prompt(`Enter number of rows
         (number between 1 and 100)`,4);
     }
-     
+    */
+
+    //square version
+    while(noCol<=0 || noCol>100 || noRow<=0 || noRow>100){
+        noCol = prompt(`Enter the canvas size
+        (number between 1 and 100)`,10);
+        noRow = noCol;
+    }
+
     setup(noCol,noRow);
 });
